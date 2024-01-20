570b5a489fddc1283e82d18733b4ae7df8badc1b
Sonu-Hansda <sonukumarhansda61@gmmail.com>
2024-01-20 12:38:25 +0530

    modified solution

--- index.js
+++ index.js
@@ -14,12 +14,28 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
-app.get("/api/:date?", (req, res) => {
-  const date = req.params.date;
-  res.json({
-    unix: new Date(date).getTime(),
-    utc: new Date(date).toUTCString(),
-  });
+
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+  let unix = 0;
+  let utc = "";
+  if (date) {
+    if (date.match(/^\d+$/)) {
+      unix = parseInt(date);
+      utc = new Date(unix).toUTCString();
+    } else {
+      unix = Date.parse(date);
+      utc = new Date(date).toUTCString();
+    }
+  } else {
+    unix = Date.now();
+    utc = new Date().toUTCString();
+  }
+  if (utc === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: unix, utc: utc });
+  }
 });
 
 const listener = app.listen(process.env.PORT, function () {
